0b3ddae5c97b74897fd39b2ef4518ec081f37f52
SDen4 <asw325@yandex.ru>
2022-06-23 09:55:58 +0500

    post-request example

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(server.json());
 app.use(cors());
 app.use(server.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}. Open http://localhost:8001/`);
 });
 
 // http://localhost:8001/api/test?testParam=test
@@ -16,5 +16,14 @@ app.get('/api/test', (req, res) => {
     res.status(200).send({ test: 'success!!!' });
   } else {
     res.status(204).send({ code: '204', message: 'no data' });
+  }
+});
+
+// пример POST-запроса (проверять с помощью postman или аналогов)
+app.post('/api/post-test', (req, res) => {
+  if (req.body.testParam === 'test') {
+    res.status(200).send({ testParam: 'success!!!' });
+  } else {
+    res.status(500).send({ code: '500', message: 'server error' });
   }
 });
