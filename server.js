d43e75d9ceeea3215016b88c23b3de3822022f5c
SDen4 <asw325@yandex.ru>
2023-05-05 22:12:35 +0500

    chao chao mock

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
 const server = require('express');
 const cors = require('cors');
 const app = server();
-const PORT = 8001;
+const PORT = 3001;
 
 app.use(server.json());
 app.use(cors());
 app.use(server.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}. Open http://localhost:8001/`);
+  console.log(`Listening on port ${PORT}. Open http://localhost:3001/`);
 });
 
-// http://localhost:8001/api/test?testParam=test
-app.get('/api/test', (req, res) => {
-  if (req.query.testParam === 'test') {
-    res.status(200).send({ test: 'success!!!' });
+// http://localhost:8001/authorize
+app.post('/authorize', (req, res) => {
+  if (req.body.username === '12' && req.body.password === '12') {
+    res.status(200).send({
+      'authorization_token': 'string',
+      'role': 'USER',
+    });
+  } else if (req.body.username === '13' && req.body.password === '13') {
+    res.status(200).send({
+      'authorization_token': 'string',
+      'role': 'ADMIN',
+    });
   } else {
-    res.status(204).send({ code: '204', message: 'no data' });
+    res.status(403).send();
   }
 });
 
-// пример POST-запроса (проверять с помощью postman или аналогов)
-app.post('/api/post-test', (req, res) => {
-  if (req.body.testParam === 'test') {
+app.get('/api', (req, res) => {
+  if (req.query.test === 'test') {
+    res.status(200).send({ testParam: 'success!!!' });
+  } else {
+    res.status(500).send({ code: '500', message: 'server error' });
+  }
+});
+
+app.post('/api', (req, res) => {
+  if (req.body.test === 'test') {
+    res.status(200).send({ testParam: 'success!!!' });
+  } else {
+    res.status(500).send({ code: '500', message: 'server error' });
+  }
+});
+
+app.post('/projects', (req, res) => {
+  if (req.body.name === '12' && req.body.description === '12') {
     res.status(200).send({ testParam: 'success!!!' });
   } else {
     res.status(500).send({ code: '500', message: 'server error' });
