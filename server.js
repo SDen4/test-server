de4ab33f2e35132ff34e0517850f1dd661a92a1f
SDen4 <asw325@yandex.ru>
2023-05-20 20:03:31 +0500

    chao-chao project data

--- server.js
+++ server.js
@@ -50,3 +50,73 @@ app.post('/projects', (req, res) => {
     res.status(500).send({ code: '500', message: 'server error' });
   }
 });
+
+const user_projects = require('./data/user_projects.json');
+app.get('/projects', (req, res) => {
+  res.status(200).send(user_projects);
+});
+
+const project12 = require('./data/project12.json');
+app.get('/projects/12/', (req, res) => {
+  res.status(200).send(project12);
+});
+
+app.post('/projects/12/stop', (req, res) => {
+  res.status(200).send();
+});
+app.post('/projects/12/start', (req, res) => {
+  res.status(200).send();
+});
+app.post('/projects/13/rebuild', (req, res) => {
+  res.status(200).send();
+});
+
+// ADMIN
+const adminInit = require('./data/adminInit.json');
+app.get('/projects/adminView', (req, res) => {
+  res.status(200).send(adminInit);
+});
+
+// USERS
+const users = require('./data/users.json');
+app.get('/users', (req, res) => {
+  res.status(200).send(users);
+});
+
+// ADD NEW USER
+app.post('/users', (req, res) => {
+  if (req.body.name === 'ivan') {
+    res.status(200).send();
+  } else {
+    res.status(500).send();
+  }
+});
+// EDIT USER
+app.put('/users', (req, res) => {
+  if (
+    req.body.availibleCpu === 10 &&
+    req.body.availibleRam === 20 &&
+    req.body.availibleStorage === 100 &&
+    req.body.createdDate === '2023-05-01T14:30:00.000-0700' &&
+    req.body.currentCpu === 1 &&
+    req.body.currentRam === 9 &&
+    req.body.currentStorage === 18 &&
+    req.body.id === 1 &&
+    req.body.name === 'user1' &&
+    req.body.requestCpu === 8 &&
+    req.body.requestRam === 15 &&
+    req.body.requestStorage === 50
+  ) {
+    res.status(200).send();
+  } else {
+    res.status(500).send();
+  }
+});
+
+// LOGS
+app.get('/projects/12/logs', (req, res) => {
+  res.status(200).send({
+    value:
+      "2019-03-05 10:57:51.112  INFO 45469 --- [           main] org.apache.catalina.core.StandardEngine  : Starting Servlet Engine: Apache Tomcat/7.0.52 \n 2019-03-05 10:57:51.253  INFO 45469 --- [ost-startStop-1] o.a.c.c.C.[Tomcat].[localhost].[/]       : Initializing Spring embedded WebApplicationContext \n 2019-03-05 10:57:51.253  INFO 45469 --- [ost-startStop-1] o.s.web.context.ContextLoader            : Root WebApplicationContext: initialization completed in 1358 ms \n 2019-03-05 10:57:51.698  INFO 45469 --- [ost-startStop-1] o.s.b.c.e.ServletRegistrationBean        : Mapping servlet: 'dispatcherServlet' to [/] \n 2019-03-05 10:57:51.702  INFO 45469 --- [ost-startStop-1] o.s.b.c.embedded.FilterRegistrationBean  : Mapping filter: 'hiddenHttpMethodFilter' to: [/*] UnauthorizedError: description: Unauthorized access error",
+  });
+});
